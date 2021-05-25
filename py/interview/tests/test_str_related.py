f6c4c94c8a961114edd84ae686f9d08a5d3d0fd6
JadenLiuu <r35191007@gmail.com>
2021-05-25 23:45:52 +0800

    add: del_to_ABstr 'AB' string deleter

--- py/interview/tests/test_str_related.py
+++ py/interview/tests/test_str_related.py
@@ -7,3 +7,14 @@ def test_del_str_costs(func=Questions.del_str_costs):
     assert 21 == func('abccccddccc', [0,0,1,2,7,3,5,8,1,9,20])
     assert 3 == func('aaaaa', [0,0,1,2,7])
     assert 0 == func('', [])
+
+def test_del_to_ABstr(func=Questions.del_to_ABstr):
+    assert 2 == func('BBAAAA')
+    assert 2 == func('BAABBBAB')
+    assert 0 == func('AABBBBBBBB')
+    assert 0 == func('')
+    assert 3 == func('ABABAAABBBA')
+    assert 1 == func('AABAB')
+    assert 3 == func('ABABABAB')
+    assert 2 == func('BAAABAAB')
+    
